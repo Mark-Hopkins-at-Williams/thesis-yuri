8ea962b6d6657d095f3ef44565c4e2de5104786f
Mark Hopkins <mh24@williams.edu>
2025-07-01 13:16:31 -0400

    Changes to experiment scripts.

--- scripts/generate_experiment2.py
+++ scripts/generate_experiment2.py
@@ -1,25 +1,41 @@
 import json
 
+VARIANT = 2
+
+if VARIANT == 1:
+    SRC = "en"
+    TGT1 = "es"
+    TGT2 = "pt"
+    SRC_ID = "eng_Latn"
+    TGT1_ID = "tsn_Latn"
+    TGT2_ID = "tso_Latn"
+elif VARIANT == 2:
+    SRC = "en"
+    TGT1 = "de"
+    TGT2 = "nl"
+    SRC_ID = "eng_Latn"
+    TGT1_ID = "tsn_Latn"
+    TGT2_ID = "tso_Latn"
 
 def create_bituning_config1(num_train_lines):
     return {
-        "model_dir": f"experiments/exp2-bi1-{num_train_lines}",
+        "model_dir": f"experiments/exp2-{VARIANT}-bi1-{num_train_lines}",
         "corpora": {
             "eng_Latn": {
-                "train": "data/train.en",
-                "dev": "data/dev.en",
-                "test": "data/test.en",
+                "train": f"data/train.{SRC}",
+                "dev": f"data/dev.{SRC}",
+                "test": f"data/test.{SRC}",
                 "permutation": 0,
             },
             "tsn_Latn": {
-                "train": "data/train.es",
-                "dev": "data/dev.es",
-                "test": "data/test.es",
+                "train": f"data/train.{TGT1}",
+                "dev": f"data/dev.{TGT1}",
+                "test": f"data/test.{TGT1}",
                 "permutation": 1,
             },
         },
         "bitexts": [
-            {"src": "eng_Latn", "tgt": "tsn_Latn", "train_lines": [0, num_train_lines]}
+            {"src": SRC_ID, "tgt": TGT1_ID, "train_lines": [0, num_train_lines]}
         ],
         "finetuning_parameters": {
             "base_model": "facebook/nllb-200-distilled-600M",
@@ -31,25 +47,25 @@ def create_bituning_config1(num_train_lines):
 
 def create_bituning_config2(num_train_lines):
     return {
-        "model_dir": f"experiments/exp2-bi2-{num_train_lines}",
+        "model_dir": f"experiments/exp2-{VARIANT}-bi2-{num_train_lines}",
         "corpora": {
             "eng_Latn": {
-                "train": "data/train.en",
-                "dev": "data/dev.en",
-                "test": "data/test.en",
+                "train": f"data/train.{SRC}",
+                "dev": f"data/dev.{SRC}",
+                "test": f"data/test.{SRC}",
                 "permutation": 0,
             },
             "tso_Latn": {
-                "train": "data/train.pt",
-                "dev": "data/dev.pt",
-                "test": "data/test.pt",
+                "train": f"data/train.{TGT2}",
+                "dev": f"data/dev.{TGT2}",
+                "test": f"data/test.{TGT2}",
                 "permutation": 1,
             },
         },
         "bitexts": [
             {
-                "src": "eng_Latn",
-                "tgt": "tso_Latn",
+                "src": SRC_ID,
+                "tgt": TGT2_ID,
                 "train_lines": [num_train_lines, 2 * num_train_lines],
             }
         ],
@@ -63,32 +79,32 @@ def create_bituning_config2(num_train_lines):
 
 def create_multituning_config(num_train_lines):
     return {
-        "model_dir": f"experiments/exp2-multi-{num_train_lines}",
+        "model_dir": f"experiments/exp2-{VARIANT}-multi-{num_train_lines}",
         "corpora": {
             "eng_Latn": {
-                "train": "data/train.en",
-                "dev": "data/dev.en",
-                "test": "data/test.en",
+                "train": f"data/train.{SRC}",
+                "dev": f"data/dev.{SRC}",
+                "test": f"data/test.{SRC}",
                 "permutation": 0,
             },
             "tsn_Latn": {
-                "train": "data/train.es",
-                "dev": "data/dev.es",
-                "test": "data/test.es",
+                "train": f"data/train.{TGT1}",
+                "dev": f"data/dev.{TGT1}",
+                "test": f"data/test.{TGT1}",
                 "permutation": 1,
             },
             "tso_Latn": {
-                "train": "data/train.pt",
-                "dev": "data/dev.pt",
-                "test": "data/test.pt",
+                "train": f"data/train.{TGT2}",
+                "dev": f"data/dev.{TGT2}",
+                "test": f"data/test.{TGT2}",
                 "permutation": 1,
             },
         },
         "bitexts": [
-            {"src": "eng_Latn", "tgt": "tsn_Latn", "train_lines": [0, num_train_lines]},
+            {"src": SRC_ID, "tgt": TGT1_ID, "train_lines": [0, num_train_lines]},
             {
-                "src": "eng_Latn",
-                "tgt": "tso_Latn",
+                "src": SRC_ID,
+                "tgt": TGT2_ID,
                 "train_lines": [num_train_lines, 2 * num_train_lines],
             },
         ],
@@ -114,24 +130,24 @@ def create_shell_script(num_train_lines):
         ]
     )
     calls = "\n".join([
-        f"python finetune.py --config configs/experiment2.bi1.{num_train_lines}.json",
-        f"python finetune.py --config configs/experiment2.bi2.{num_train_lines}.json",
-        f"python finetune.py --config configs/experiment2.multi.{num_train_lines}.json",
+        f"python finetune.py --config configs/experiment2.{VARIANT}.bi1.{num_train_lines}.json",
+        f"python finetune.py --config configs/experiment2.{VARIANT}.bi2.{num_train_lines}.json",
+        f"python finetune.py --config configs/experiment2.{VARIANT}.multi.{num_train_lines}.json",
     ])
     return preface + "\n" + calls
 
 
 for num_train_lines in [256, 512, 1024, 2048, 4096, 8192, 16834]:
     config = create_bituning_config1(num_train_lines)
-    with open(f"configs/experiment2.bi1.{num_train_lines}.json", "w") as writer:
+    with open(f"configs/experiment2.{VARIANT}.bi1.{num_train_lines}.json", "w") as writer:
         json.dump(config, writer, indent=4)
     config = create_bituning_config2(num_train_lines)
-    with open(f"configs/experiment2.bi2.{num_train_lines}.json", "w") as writer:
+    with open(f"configs/experiment2.{VARIANT}.bi2.{num_train_lines}.json", "w") as writer:
         json.dump(config, writer, indent=4)
     config = create_multituning_config(num_train_lines)
-    with open(f"configs/experiment2.multi.{num_train_lines}.json", "w") as writer:
+    with open(f"configs/experiment2.{VARIANT}.multi.{num_train_lines}.json", "w") as writer:
         json.dump(config, writer, indent=4)
     shell_script = create_shell_script(num_train_lines)
-    with open(f"configs/run.exp2.{num_train_lines}.json", "w") as writer:
+    with open(f"configs/run.exp2.{VARIANT}.{num_train_lines}.json", "w") as writer:
         writer.write(shell_script)
     
--- scripts/plot_scores.py
+++ scripts/plot_scores.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 BASE_DIR = Path("experiments/")
+PREFIX = "exp1"
+METRIC = "bleu"
 
 def mean(ls):
     return sum(ls) / len(ls)
 
 def read_scores(experiment_dir):
+    print(experiment_dir)
     try:
         with open(Path(experiment_dir) / "scores.json") as reader:
             data = json.load(reader)
@@ -15,17 +18,17 @@ def read_scores(experiment_dir):
         data = None
     return data
 
-x = [256, 512, 1024, 2048, 4096, 8192, 16834]
+x = [512, 1024, 2048, 4096, 8192, 16834]
 ys = dict()
 for tuning in ['multi', 'bi1', 'bi2']:
     ys[tuning] = []
     for num_train_lines in x:
-        exp_dirs = list(BASE_DIR.glob(f"exp1-{tuning}-{num_train_lines}-v*")) 
+        exp_dirs = list(BASE_DIR.glob(f"{PREFIX}-{tuning}-{num_train_lines}-v*")) 
         trial_scores = []
         for exp_dir in exp_dirs:
             scores = read_scores(exp_dir)
             if scores is not None:
-                score_avg = mean([scores[lang_pair]['chrf'] for lang_pair in scores])
+                score_avg = mean([scores[lang_pair][METRIC] for lang_pair in scores])
                 trial_scores.append(score_avg)
         ys[tuning].append(mean(trial_scores))
 
@@ -33,8 +36,9 @@ y_bi = [(y1+y2)/2 for (y1, y2) in zip(ys['bi1'], ys['bi2'])]
 
 plt.plot(x, y_bi, label='bi', color='blue', linestyle='-')
 plt.plot(x, ys['multi'], label='multi', color='red', linestyle='--')
+plt.xscale('log')
 plt.xlabel('num train')
-plt.ylabel('BLEU')
-plt.title('Experiment 1')
+plt.ylabel(METRIC)
+plt.title('Experiment')
 plt.legend()
-plt.savefig('exp1.chrf.png')
+plt.savefig(f'{PREFIX}.{METRIC}.png')
