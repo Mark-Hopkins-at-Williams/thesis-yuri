803db4f546921c018d322bcaa7f3a70356f6a711
Mark Hopkins <mh24@williams.edu>
2025-10-20 11:48:08 -0400

    Temporarily adding trystuff.py for reference.

---
+++ trystuff.py
@@ -0,0 +1,27 @@
+import json
+import unittest
+from corpora import load_tokenizer, Bitext, MixtureOfBitexts, TokenizedMixtureOfBitexts
+from torch import tensor
+from transformers import AutoModelForSeq2SeqLM
+
+bitext = Bitext("test_files/generate.txt", "test_files/lang2.txt")
+mix = MixtureOfBitexts({(("test", "eng"), ("test", "fra")): bitext}, 20)
+#print(mix.next_batch())
+
+
+lang_codes = {
+    ("test", "eng"): "eng_Latn",
+    ("test", "fra"): "fra_Latn"
+}
+base_model = "facebook/nllb-200-distilled-600M"
+tokenizer = load_tokenizer(base_model)
+tmob = TokenizedMixtureOfBitexts(mix, tokenizer, lang_codes=lang_codes, max_length=128)
+model = AutoModelForSeq2SeqLM.from_pretrained(base_model) 
+
+x, y, _, _ = tmob.next_batch()
+
+x = x.to(model.device)
+y = y.to(model.device)
+loss = model(**x, labels=y.input_ids).loss
+print(loss)
+
